f75a016a4e652d237349d6c6281d14571ee90870
Walatown <polina.terentjeva11@gmail.com>
2025-10-21 23:47:07 +0700

    Migrating files from Gitlab to Github

---
+++ ArtDetective/ArtDetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtDetectiveApp.swift
+//  ArtDetective
+//
+//  Created by Magda Tsekova on 19/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtDetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
